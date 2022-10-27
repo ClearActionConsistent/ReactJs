55b41626241faafef0fb2118d439991031ad5738
phantuananh18 <tuananh.61779@gmail.com>
2022-10-27 15:33:23 +0700

    Create header component
    - Config router
    - install package react-redux to state management

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./views/components/Home";
+import AboutPage from "./views/components/About";
+import NotFoundPage from "./views/components/NotFound";
 
 const App = () => {
-  return <div>We are best team</div>;
+  return (
+    <Routes>
+      <Route path="/home" exact element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  )
 };
 
 export default App;
