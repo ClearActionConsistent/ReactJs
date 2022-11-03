006a94161bd2de8dea4011057397791142d4b47e
ClearActionConsistent <51291390+ClearActionConsistent@users.noreply.github.com>
2022-11-03 17:07:01 +0700

    Update index.js

--- src/views/components/Header/index.js
+++ src/views/components/Header/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ title, className, onClick }) => {
     return (
         <div className='header'>
-            Task Tracker From Main Branch 
+            Task Tracker From Main Branch v1 
             <button className={className} onClick={onClick}>{title}</button>
         </div>
     )
