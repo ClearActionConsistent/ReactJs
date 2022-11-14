925d6cb5b80a91346fdd503bac90f79cbd98a1b1
Duc Vo <duclionel0702@gmail.com>
2022-11-14 15:46:58 +0700

    feat/Community

---
+++ src/views/components/Posts/profile.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class Profile extends Component {
+    render() {
+        const { value } = this.props
+        return (
+            <>
+                <div className='profile_containter'>
+                    <img src={value.avatar || null} alt="avatar" />
+                    <div className='profile_detail'>
+                        <span className='name'>{value.name || null}</span>
+                        <div className='profile_location'>
+                            <img className='run' alt="run_icon" />
+                            <span className='location'>
+                                {value.time || null} . {value.location || null}
+                            </span>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='date'>
+                    {value.session || null}
+                </div>
+            </>
+        )
+    }
+}
+
+export default Profile
