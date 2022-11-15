6f7e7a2596a3b0d718f03649bb71c8439d559df2
Duc Vo <duclionel0702@gmail.com>
2022-11-15 10:10:49 +0700

    feat/Fetch data for classes

--- src/views/components/Classes/listItem.jsx
+++ src/views/components/Classes/listItem.jsx
@@ -1,16 +1,19 @@
 
 import React, { Component } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import ItemCard from './itemcard'
-
+import { classes } from "../../../data"
 
 class ListItem extends Component {
 
     render() {
         return (
             <div className='lst_item'>
-                <ItemCard />
-                <ItemCard />
-                <ItemCard />
+                {
+                    classes.run.map((item) => {
+                        return <ItemCard value={item} key={uuidv4()} />
+                    })
+                }
             </div>
         )
     }
