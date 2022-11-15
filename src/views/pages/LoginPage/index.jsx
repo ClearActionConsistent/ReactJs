0f0ea2c9e09914a2f14fec579c58d308f2dbd90b
John Nguyen <clear.action.consistent@gmail.com>
2022-11-15 13:15:13 +0700

    convert login page to function based component

--- src/views/pages/LoginPage/index.jsx
+++ src/views/pages/LoginPage/index.jsx
@@ -1,105 +1,80 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import FormInput from '../../components/FormInput';
 import './styles.css';
 
-class LoginPage extends Component {
-    constructor(props) {
-        super(props);
-        this.LoginRequest = this.LoginRequest.bind(this);
-        this.ChangeEmail = this.ChangeEmail.bind(this);
-        this.ChangePassword = this.ChangePassword.bind(this);
-        this.handleClickShowPassword = this.handleClickShowPassword.bind(this);
-        this.state = {
-            isLoggedIn: false,
-            email: '',
-            password: '',
-            isHide: false,
-            error: false
-        }
+const LoginPage = (props) => {
+
+    //set default states
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isHide, setIsHide] = useState(false);
+    const [error, setError] = useState(false);
+
+    //events
+    const onChangeEmail = (event) => {
+        setEmail(event.target.value);
     }
 
-    componentDidMount() {
-        console.log('Login componentDidMount');
+    const onChangePassword = (event) => {
+        setPassword(event.target.value);
     }
 
-    componentWillUnmount() {
-        console.log('Login componentWillUnmount');
-        this.setState({ isLoggedIn: true });
-        this.setState({ email: '' });
-        this.setState({ password: '' });
-        this.setState({ error: false });
-        this.setState({ isHide: false });
-    }
-
-    ChangeEmail(event) {
-        const value = event.target.value;
-        this.setState({ email: value });
-    }
-
-    ChangePassword(event) {
-        const value = event.target.value;
-        this.setState({ password: value });
-    }
-
-    LoginRequest() {
-        if (this.state.email === 'tuananh@gmail.com' && this.state.password === '12345678') {
-            this.setState({ isShow: false });
-            this.setState({ isLoggedIn: true });
-            this.setState({ error: false });
+    const LoginRequest = () => {
+        if (email === 'tuananh@gmail.com' && password === '12345678') {
+            setIsLoggedIn(true);
+            setError(false);
         }
         else {
-            this.setState({ error: true });
-            this.setState({ isShow: true });
-            this.setState({ isLoggedIn: false });
-            this.setState({ email: '' });
-            this.setState({ password: '' });
+            setError(true);
+            setIsLoggedIn(false);
+            setEmail('');
+            setPassword('');
         }
     }
 
-    handleClickShowPassword() {
-        this.setState({ isHide: !this.state.isHide });
+    const handleClickShowPassword = () => {
+        setIsHide(!isHide);
     };
 
-    render() {
-        return (
-            <div className='container'>
-                <div className='img-logo'>
-                    <img style={{ height: '300px' }} src='https://www.fitnesstogether.gr/wp-content/uploads/2021/06/Kick-Boxing-1-1024x529.jpg' alt='GTT logo' />
+    return (
+        <div className='container'>
+            <div className='img-logo'>
+                <img style={{ height: '300px' }} src='https://www.fitnesstogether.gr/wp-content/uploads/2021/06/Kick-Boxing-1-1024x529.jpg' alt='GTT logo' />
+            </div>
+            <div className='form-login'>
+                <div className='login'>
+                    <p>Already have an account?</p>
+                    <FormInput
+                        email={email}
+                        password={password}
+                        changeEmail={onChangeEmail}
+                        changePassword={onChangePassword}
+                        error={error}
+                        isHide={isHide}
+                        onClick={handleClickShowPassword}
+                        isRegister={false}
+                    />
+                    <button className='btn btn-login' onClick={LoginRequest}>Log In</button>
+                    {isLoggedIn && <Navigate to='/home' />}
+                    <Link to='/register' className='sign-up'>New to GTT? Sign up.</Link>
                 </div>
-                <div className='form-login'>
-                    <div className='login'>
-                        <p>Already have an account?</p>
-                        <FormInput
-                            email={this.state.email}
-                            password={this.state.password}
-                            changeEmail={this.ChangeEmail}
-                            changePassword={this.ChangePassword}
-                            error={this.state.error}
-                            isHide={this.state.isHide}
-                            onClick={this.handleClickShowPassword}
-                            isRegister={false}
-                        />
-                        <button className='btn btn-login' onClick={() => this.LoginRequest()}>Log In</button>
-                        {this.state.isLoggedIn && <Navigate to='/home' />}
-                        <Link to='/register' className='sign-up'>New to GTT? Sign up.</Link>
-                    </div>
-                    <div className='login-with'>
-                        <button className='btn login-with__facebook'>
-                            <div className='icon-facebook'><i className='fab fa-facebook'></i></div>
-                            <p>Continue with Facebook</p>
-                            <div></div>
-                        </button>
-                        <button className='btn login-with__google'>
-                            <span className='icon-google'></span>
-                            <p>Continue with Google</p>
-                            <div></div>
-                        </button>
-                    </div>
+                <div className='login-with'>
+                    <button className='btn login-with__facebook'>
+                        <div className='icon-facebook'><i className='fab fa-facebook'></i></div>
+                        <p>Continue with Facebook</p>
+                        <div></div>
+                    </button>
+                    <button className='btn login-with__google'>
+                        <span className='icon-google'></span>
+                        <p>Continue with Google</p>
+                        <div></div>
+                    </button>
                 </div>
-            </div >
-        )
-    }
+            </div>
+        </div >
+    )
 }
 
 export default LoginPage;
