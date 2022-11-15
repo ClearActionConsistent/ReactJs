5b30f174b6aa79124dde91c585ebeb89eac66622
John Nguyen <clear.action.consistent@gmail.com>
2022-11-15 14:08:55 +0700

    convert router to function based component

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AboutPage from '../views/pages/AboutPage';
 import LoginPage from '../views/pages/LoginPage';
@@ -11,43 +11,27 @@ import ChallengeBoard from '../views/components/ChallengeBoard';
 import WorkoutHistory from '../views/components/WorkoutHistory';
 import ListPost from '../views/components/Posts';
 
-class Router extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-    };
-  }
+const Router = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false); 
 
-  componentDidMount() {
-    console.log('Router componentDidMount');
-  }
-
-  componentWillUnmount() {
-    console.log('Router componentWillUnmount');
-  }
-
-  render() {
-    const loggedIn = this.state.isLoggedIn;
-    return (
-      <Routes>
-        <Route
-          exact
-          path='/'
-          element={<PrivateRoute isLoggedIn={loggedIn} />}
-        />
-        <Route path='/home' element={<HomePage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<SignupPage />} />
-        <Route path='/about' element={<AboutPage />} />
-        <Route path='/classes' element={<Classes />} />
-        <Route path='/subscribe' element={<Subscribe />} />
-        <Route path='/workout/challenge' element={<ChallengeBoard />} />
-        <Route path='/workout' element={<WorkoutHistory />} />
-        <Route path='/community' element={<ListPost />} />
-      </Routes>
-    );
-  }
+  return (
+    <Routes>
+      <Route
+        exact
+        path='/'
+        element={<PrivateRoute isLoggedIn={isLoggedIn} />}
+      />
+      <Route path='/home' element={<HomePage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<SignupPage />} />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/classes' element={<Classes />} />
+      <Route path='/subscribe' element={<Subscribe />} />
+      <Route path='/workout/challenge' element={<ChallengeBoard />} />
+      <Route path='/workout' element={<WorkoutHistory />} />
+      <Route path='/community' element={<ListPost />} />
+    </Routes>
+  );
 }
 
 export default Router;
