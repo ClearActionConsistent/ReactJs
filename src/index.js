900159fa17dd8ef8d2f7f60ad7df2dd44ad5de9b
John Nguyen <clear.action.consistent@gmail.com>
2022-11-16 16:19:40 +0700

    revert unexpected changes on index.html

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 import store from './store'
-//import { PersistGate } from 'redux-persist/integration/react';
-//import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
-//let persistor = persistStore(store);
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+    <Provider store={store}>
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
+        <App />
+    </Provider>
 );
 
