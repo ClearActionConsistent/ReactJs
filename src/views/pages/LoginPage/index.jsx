7f9daf541eb18fc720292f9f7a1f1afca226ce70
Tino Phan <80447142+tinophan247@users.noreply.github.com>
2022-11-21 18:10:36 +0700

    Fix issue

--- src/views/pages/LoginPage/index.jsx
+++ src/views/pages/LoginPage/index.jsx
@@ -64,7 +64,7 @@ const LoginPage = (props) => {
                         onClick={handleClickShowPassword}
                         isRegister={false}
                     />
-                    <button type='submit' className='btn btn-login' onClick={handleSubmit}>Log In</button>
+                    <button type='submit' className='btn btn-login'>Log In</button>
                     {isLoggedIn && <Navigate to='/home' replace />}
                     <Link to='/register' className='sign-up'>New to GTT? Sign up.</Link>
                 </form>
